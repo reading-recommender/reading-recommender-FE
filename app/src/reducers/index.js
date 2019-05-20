96288dcb8d2090b53dc517dfbb80add078ba218f
PetejStone <petejstoney@gmail.com>
2019-05-20 10:44:21 -0700

    connected to redux store

---
+++ app/src/reducers/index.js
@@ -0,0 +1,9 @@
+
+const initialState = {
+    data: []
+    // Array characters, Boolean fetching, null error.
+  };
+
+  export const rootReducer = (state=initialState, action)=> {
+      return state;
+  }
