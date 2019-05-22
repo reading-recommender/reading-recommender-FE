d1f2b5d63b2a5f8aef3d89ab7e39ff9d206ae56f
Shadowborn <james.starks023@gmail.com>
2019-05-22 16:55:50 -0600

    Adding questions to Peters login

--- reading-recommender/src/PrivateRoute.js
+++ reading-recommender/src/PrivateRoute.js
@@ -1,19 +1,25 @@
+    
 import React from "react";
 import { Route, Redirect } from "react-router-dom";
+import {connect} from 'react-redux';
 
-const PrivateRoute = ({ component: Component, ...rest }) => {
+ const PrivateRoute =  ( {component: Component,  guest, ...rest} )  => {
   return (
+    <div>  {console.log(guest)}
     <Route
       {...rest}
-      render={() => {
-        if (localStorage.getItem("token")) {
-          return <Component />;
+      render={(props) => {
+        if (localStorage.getItem("token") || guest) {
+          return <Component {...props}/>;
         } else {
           return <Redirect to="/" />;
         }
       }}
     />
-  );
+    </div>); 
 };
 
-export default PrivateRoute;
+const mapStateToProps = (state) => ({
+  guest: state.guest
+})
+export default connect(mapStateToProps,{})(PrivateRoute);
